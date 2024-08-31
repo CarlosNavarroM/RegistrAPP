a8757b0fd74cad30e4b8929ea205439e84e1fcb6
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-08-31 17:38:26 -0400

    update

--- src/app/user/profile/profile.page.ts
+++ src/app/user/profile/profile.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  navigateToEditProfile() {
+    this.router.navigate(['/edit-profile']);
   }
 
+  navigateToSettings() {
+    // Aquí podrías redirigir a una página de ajustes si la tuvieras
+    this.router.navigate(['/settings']);
+  }
 }
