2b8c40a1c80e020b9a444cdcb444cb0cf70ea20f
bllq99 <bastian.lluch@gmail.com>
2024-09-03 15:09:14 -0400

    Implementación de restablecer contraseña

--- src/app/auth/reset-password/reset-password.page.ts
+++ src/app/auth/reset-password/reset-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
+  resetPasswordForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router:Router) { }
 
   ngOnInit() {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.resetPasswordForm.valid) {
+      const email = this.resetPasswordForm.get('email')?.value;
+      console.log(email);
+    }
   }
 
 }
