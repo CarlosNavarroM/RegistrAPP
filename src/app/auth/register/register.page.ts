43a511a46f52ed9c1e4074b24ea8565e098e94fb
bllq99 <bastian.lluch@gmail.com>
2024-09-05 15:51:31 -0400

    Agregado el servicio de Firebase

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
+
+function passwordMatchValidator(form: FormGroup) {
+  const password = form.get('password')?.value;
+  const confirmPassword = form.get('confirmPassword')?.value;
+  return password === confirmPassword ? null : { mismatch: true };
+}
 
 @Component({
   selector: 'app-register',
@@ -8,28 +16,80 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  form = new FormGroup({
+    uid: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]), 
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    role: new FormControl('', [
+      Validators.required]),
+    rut: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]),
+  }, { validators: passwordMatchValidator });
+
+  fireBaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
 
   ngOnInit() {
-    this.registerForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      rut: ['', [Validators.required, Validators.minLength(8)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]],
-    }, { validator: this.passwordMatchValidator });
   }
 
-  passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value
-      ? null : { mismatch: true };
+  async submit() {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      this.fireBaseSvc.signUp(this.form.value as User).then(async res => {
+        await this.fireBaseSvc.updateUser(this.form.value.name);
+        let uid = res.user.uid;
+        this.form.controls.uid.setValue(uid);
+        this.setUserInfo(uid);
+      }).catch(err => {
+        console.error(err);
+        this.utilsSvc.presentToast({ 
+          message: err.message, 
+          duration: 2000,
+          color: 'danger',
+          position:'middle',
+          icon: 'close-circle-outline' 
+        });
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
   }
 
-  onSubmit() {
-    if (this.registerForm.valid) {
-      this.router.navigate(['/login']);
+  async setUserInfo(uid: string) {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      let path = `users/${uid}`;
+      delete this.form.value.password
+      delete this.form.value.confirmPassword;
+
+
+      this.fireBaseSvc.setDocument(path, this.form.value).then(async res => {
+        
+        this.utilsSvc.saveInLocalStorage('user', this.form.value);
+        this.utilsSvc.routerLink('/login');
+        this.form.reset();
+
+
+      }).catch(err => {
+        console.error(err);
+        this.utilsSvc.presentToast({ 
+          message: err.message, 
+          duration: 2000,
+          color: 'danger',
+          position:'middle',
+          icon: 'close-circle-outline' 
+        });
+      }).finally(() => {
+        loading.dismiss();
+      });
     }
   }
 }
