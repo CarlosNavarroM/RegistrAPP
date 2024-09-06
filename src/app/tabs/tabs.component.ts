c6691fa80cdbb96ae7b4a15b1339cd9d7fe74e0f
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-09-06 06:10:46 -0400

    update

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
 })
-export class TabsComponent {
+export class TabsComponent implements OnDestroy {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private navCtrl: NavController) {}
 
   navigateToHome() {
     this.router.navigate(['/home-docente']);
@@ -20,5 +21,10 @@ export class TabsComponent {
 
   navigateToQR() {
     this.router.navigate(['/scan-qr']);
+  }
+
+  ngOnDestroy() {
+    console.log('TabsComponent ha sido destruido');
+    
   }
 }
