8435fda85354fb62d6f01a0cb9bdfafea9922597
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2024-10-26 18:07:55 -0300

    update

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCcssRfcqQsehPkfQyKK3olt3LMwdVlr4U",
+    authDomain: "registrapp-1e71c.firebaseapp.com",
+    projectId: "registrapp-1e71c",
+    storageBucket: "registrapp-1e71c.appspot.com",
+    messagingSenderId: "866555568395",
+    appId: "1:866555568395:web:aeb4cc5a1aad0f3f0a820e"
+  }
+};
